79973c15b78cd5315cae1a4e6a3044bc8631692b
Musfiq65 <160261648+Md-Musfiq-Hossain@users.noreply.github.com>
2024-11-12 12:48:21 +0000

    init_test

---
+++ numpy/numpy_test.py
@@ -0,0 +1,61 @@
+#Array Define
+"""This is a comment"""
+import numpy as np
+
+"""
+initial_array = np.array([[1, 2, 3],
+                         [4, 5, 6]])
+
+print(initial_array)
+print(initial_array.shape)
+print(initial_array.size)
+print("type of dataset: \n ", type(initial_array))
+
+print("----------------")
+
+send_array = np.array([[[1, 2, 3],
+                      [4, 5, 6]],
+                      [[1, 2, 3],
+                      [4, 5, 6]]])
+
+print(send_array)
+print(send_array.shape)
+print(send_array.size)
+print("type of dataset: \n ", type(send_array))
+
+"""
+"""
+z_data = np.zeros((2, 2, 3))
+print(z_data)
+
+print('---------------')
+
+o_data = np.ones((2, 2, 3))
+print(o_data)
+
+print('---------------')
+
+f_data = np.full((2, 3), 6)
+print(f_data)
+
+print('---------------')
+
+e_data = np.eye(2)
+print(e_data)
+
+print('---------------')
+
+a_data = np.arange(4, 4.5, 0.1) #(start, end , step)
+print(a_data)
+
+
+print('---------------')
+
+l_data = np.linspace(1, 3, 4)
+print (l_data)
+ 
+"""
+
+# array_data = np.array([np.arange(2, 50, 3), np.arange(3, 51, 3)])
+# print (array_data[1, :])
+
